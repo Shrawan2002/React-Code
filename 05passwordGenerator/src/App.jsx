8f7b4ff5a521bed0eb6a7fe96f9b338a86326fe0
Shrawan2002 <shrawankumar1754@gmail.com>
2025-06-28 20:36:39 +0530

    initial files

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useCallback} from 'react'
+import { useState,useCallback, useEffect} from 'react'
 
 import './App.css'
 
@@ -13,13 +13,17 @@ function App() {
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if(numberAllowed) str += "0123456789";
     if(charAllowed) str += "!@#$%^&*_++[]~`";
-    for(let i = 1; i<= array.length; i++){
+    for(let i = 1; i<=length; i++){
       let char = Math.floor(Math.random()*str.length) +1;
-      pass = str.charAt(char);
+      pass += str.charAt(char);
 
     }
     setPassword(pass);
   },[length,numberAllowed,charAllowed,setPassword])
+
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,numberAllowed,charAllowed,passwordGenerator])
    return (
     <>
       {/* <div  className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8  text-orange-700 bg-gray-500'>
@@ -56,8 +60,24 @@ function App() {
       max={100}
       value={length}
       className='cursor-pointer'
+      onChange={(e)=>setLength(e.target.value)}
       />
       <label>Length:{length}</label>
+     </div>
+     <div className="flex items-center gap-x-1">
+      <input type="checkbox"  id="numberInput" 
+      defaultChecked={numberAllowed}
+      onChange={()=>setNumberAllowed((prev) => !prev)}
+      />
+      <label htmlFor='numberInput'>Number</label>
+     </div>
+     <div className="flex items-center gap-x-1">
+      <input type="checkbox" 
+      defaultChecked={charAllowed} 
+      id="characterInput" 
+      onChange={()=>setCharAllowed((prev)=>!prev)}
+      />
+      <label htmlFor='characterInput'>Characters</label>
      </div>
     </div>
   </div>
