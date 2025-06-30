03c862cb8ab89e2aa87c45eb0f1f33723bcf6888
Shrawan2002 <shrawankumar1754@gmail.com>
2025-06-30 21:19:40 +0530

    initial files

--- 06currencyConvertor/src/components/InputBox.jsx
+++ 06currencyConvertor/src/components/InputBox.jsx
@@ -28,7 +28,7 @@ export default function InputBox({
         disabled = {amountDisable}
         value={amount}
         onClick={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}
-         id="" />
+          />
         </div>
         <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className='text-black/40 mb-2 w-full'>Currency Type</p>
