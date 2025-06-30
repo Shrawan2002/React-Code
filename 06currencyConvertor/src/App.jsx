0ca11d4fc4af77865db294ef66d7869ac4c3f3a6
Shrawan2002 <shrawankumar1754@gmail.com>
2025-06-30 15:34:39 +0530

    initial files

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -6,6 +6,24 @@ import './App.css'
 function App() {
 
   const [amount,setAmount] = useState(0)
+  let [from,setFrom] = useState("usd")
+  let [to,setTo] = useState("inr")
+  const[convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  const swap = ()=>{
+    setFrom(to)
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  }
+
+  const convert = ()=>{
+    setConvertedAmount(amount*currencyInfo[to])
+  }
   return (
     <>
     <h1 className='text-3xl bg-yellow-600'>Currency app with chai </h1>
