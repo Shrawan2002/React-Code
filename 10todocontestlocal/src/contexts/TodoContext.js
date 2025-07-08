798a0f878ee57e31969804a52577254f1461e67d
Shrawan2002 <shrawankumar1754@gmail.com>
2025-07-08 11:58:02 +0530

    initial files

---
+++ 10todocontestlocal/src/contexts/TodoContext.js
@@ -0,0 +1,18 @@
+import { createContext,useContext } from "react";
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id:1,
+            todo: "Todo msg",
+            completed: false
+        }
+    ]
+})
+
+
+export const useTodo = ()=>{
+    return useContext(TodoContext);
+}
+
+export const todoprovider = TodoContext.Provider;
