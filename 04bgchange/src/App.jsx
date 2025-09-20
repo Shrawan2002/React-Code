d153cb2c5373f1d59feee977da394a53000392a3
Shrawan2002 <shrawankumar1754@gmail.com>
2025-09-20 19:21:35 +0530

    update app

--- 04bgchange/src/App.jsx
+++ 04bgchange/src/App.jsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <>
+    <div className="w-full h-screen duration-200"  style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>text</div>
+    </div>
     </>
   )
 }
