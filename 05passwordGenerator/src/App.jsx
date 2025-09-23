647944c2cad6b7a135c6eb5029a7e2c62a5d6903
Shrawan2002 <shrawankumar1754@gmail.com>
2025-09-23 12:19:35 +0530

    update password generator fun

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,12 +1,17 @@
  
 import './App.css'
-import { useRef, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 
 function App() {
   let [length, setLength] = useState(8);
   let [numberAllowed, setNumberAllowed] = useState(false);
   let [characterAllowed, setCharacterAllowed] = useState(false);
   let [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(()=>{
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghtijklmnopqrstuvwxyz"
+  }, [length, numberAllowed, characterAllowed])
   return (
     <>
     <div className='bg-red-600'>
