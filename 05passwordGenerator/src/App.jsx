c3510e12fbb370c2117ebe5ae6c4f38967622dad
Shrawan2002 <shrawankumar1754@gmail.com>
2025-09-24 21:33:18 +0530

    update

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,24 +1,24 @@
  
-// import './App.css'
-// import { useCallback, useRef, useState } from 'react';
+import './App.css'
+import { useCallback, useRef, useState } from 'react';
 
-// function App() {
-//   let [length, setLength] = useState(8);
-//   let [numberAllowed, setNumberAllowed] = useState(false);
-//   let [characterAllowed, setCharacterAllowed] = useState(false);
-//   let [password, setPassword] = useState("");
+function App() {
+  let [length, setLength] = useState(8);
+  let [numberAllowed, setNumberAllowed] = useState(false);
+  let [characterAllowed, setCharacterAllowed] = useState(false);
+  let [password, setPassword] = useState("");
 
-//   const passwordGenerator = useCallback(()=>{
-//     let pass = "";
-//     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghtijklmnopqrstuvwxyz"; 
-//   }, [length, numberAllowed, characterAllowed])
-//   return (
-//     <>
-//     <div className='bg-red-600'>
-//       hello 
-//     </div>
-//     </>
-//   )
-// }
+  const passwordGenerator = useCallback(()=>{
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghtijklmnopqrstuvwxyz"; 
+  }, [length, numberAllowed, characterAllowed])
+  return (
+    <>
+    <div className='bg-red-600'>
+      hello 
+    </div>
+    </>
+  )
+}
 
-// export default App
+export default App
