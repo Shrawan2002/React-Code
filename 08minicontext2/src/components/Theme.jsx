d17a3a09b311d5d3be421c642a501fa2dcf2afd8
Shrawan2002 <shrawankumar1754@gmail.com>
2025-10-10 17:42:01 +0530

    create theme button

--- 08minicontext2/src/components/Theme.jsx
+++ 08minicontext2/src/components/Theme.jsx
@@ -1,22 +1,22 @@
-import { useContext } from "react"
-import { ThemeContext } from "../App"
+// import { useContext } from "react"
+// import { ThemeContext } from "../App"
 
-const Theme = ()=>{
+// const Theme = ()=>{
 
-    let {theme, setTheme} = useContext(ThemeContext);
+//     let {theme, setTheme} = useContext(ThemeContext);
 
-    const changeTheme = ()=>{
-        if(theme === "light"){
-            setTheme("dark");
-        }else{
-            setTheme("light");
-        }
-    }
-    return(
-        <div>
-            <button onClick={changeTheme}>Change Theme</button>
-        </div>
-    )
-}
+//     const changeTheme = ()=>{
+//         if(theme === "light"){
+//             setTheme("dark");
+//         }else{
+//             setTheme("light");
+//         }
+//     }
+//     return(
+//         <div>
+//             <button onClick={changeTheme}>Change Theme</button>
+//         </div>
+//     )
+// }
 
-export default Theme;
+// export default Theme;
