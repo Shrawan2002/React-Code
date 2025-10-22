ccb65a5bb9b39bab2710e252c836122448711920
Shrawan2002 <shrawankumar1754@gmail.com>
2025-10-22 20:44:47 +0530

    redux

--- 13useReducer/src/App.jsx
+++ 13useReducer/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useReducer } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+const reducer = (state, action)=>{
+  if(action.type === 'INCREMENT'){
+     return state + 1;
+  }else{
+    return state - 1;
+  }
+}
+
+  const [count, dispatch] = useReducer(reducer, 0);
+  console.log(count)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <p style={{padding: "10px" , backgroundColor: "bisque"}}>{count}</p>
+      <button onClick={()=> dispatch({type: "INCREMENT"})} style={{padding: "8px", marginRight: "8px", backgroundColor: "blue", color: "white"}}>Increment</button>
+
+      <button onClick={()=> dispatch({type: "DECREMENT"})} style={{padding: "8px", backgroundColor: "red", color: "white"}}>decrement</button>
+    </div>
+
   )
 }
 
